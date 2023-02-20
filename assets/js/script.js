c2a0182a32a1ce43128006d002c0bdc0b3b6c0c3
kh.saint44 <119610043+44-khowell@users.noreply.github.com>
2023-02-20 17:09:44 +0000

    WIP 5-day forecast placeholders created + API fetch coded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,51 @@
 
+
+
+
+// Build the query URL for the ajax request to the NYT API
+
+// Query URL Working Format 
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=51.507322&lon=-0.127647&appid=fc1a788e45551d795ccebca44d61a4ea";
+
+const apiKey = "fc1a788e45551d795ccebca44d61a4ea";
+
+let coOrdLon = -0.127647;
+let coOrdLat = 51.507322;
+
+// let openWeatherQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=51.507322&lon=-0.127647&appid="+apiKey;
+
+let openWeatherQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+coOrdLat+"&lon="+coOrdLon+"&appid="+apiKey;
+
+
+fetch(openWeatherQueryUrl)
+.then(response => response.json())
+.then(result => {
+    console.log(result);
+    console.log(result.city.name);
+
+    let tempcalc = (result.list[0].main.temp - 32)*5/9 -128;
+    console.log('temp:',tempcalc.toFixed(2));
+    console.log(result.list[0].wind.speed);
+    console.log(result.list[0].main.humidity);
+    console.log(JSON.stringify(result.list[0].main.temp));
+
+
+})
+
+// const apiKey = "057aa2c42e8e4730af75e101b91db1a7";
+//   console.log("City: "+cityName);
+//   let geoQueryURL = "https://api.geoapify.com/v1/geocode/search?text="+cityName;
+
+//   // get placeID from Geocoding API
+//   fetch(geoQueryURL+"&apiKey="+apiKey, requestOptions)
+//   .then(groResponse => groResponse.json())
+//   .then(geoResult => {
+//     console.log(geoResult);
+
+
+
+
+
+// CLICK HANDLERS
+// ==========================================================
+
